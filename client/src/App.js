fb41abcb9d93f9f66443ea6abaf4418a843d1948
Mariia Batanova <batanova708@gmail.com>
2023-05-08 16:07:36 +0300

    Lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-import UserProfile from './components/UserProfile';
-import HomePage from './pages/HomePage';
-import UserPage from './pages/UserPage';
-import LogUpPage from './pages/LogUpPage';
-import GroupsPage from './pages/GroupsPage';
+// import UserProfile from './components/UserProfile';
+// import HomePage from './pages/HomePage';
+// import UserPage from './pages/UserPage';
+// import LogUpPage from './pages/LogUpPage';
+// import GroupsPage from './pages/GroupsPage';
+const HomePage = lazy(() => import('./pages/HomePage'));
+const UserPage = lazy(() => import('./pages/UserPage'));
+const LogUpPage = lazy(() => import('./pages/LogUpPage'));
+const GroupsPage = lazy(() => import('./pages/GroupsPage'));
+const UserProfile = lazy(() => import('./components/UserProfile'));
 
 const App = () => {
   return (
@@ -25,13 +30,15 @@ const App = () => {
           </li>
         </ol>
       </nav>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/users" element={<UserPage />} />
-        <Route path="/group-create" element={<GroupsPage />} />
-        <Route path="/log-up" element={<LogUpPage />} />
-        <Route path="/users/:idUser" element={<UserProfile />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/users" element={<UserPage />} />
+          <Route path="/group-create" element={<GroupsPage />} />
+          <Route path="/log-up" element={<LogUpPage />} />
+          <Route path="/users/:idUser" element={<UserProfile />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
